a96f5efac2bb0c75200aa477d538f6cf16576098
Chris Zichko <chriszichkocoding@gmail.com>
2023-07-14 23:20:53 -0400

    Added a star background that allows for stars to move when scrolling but only works going south.

--- zichkoding-portfolio/src/pages/index.tsx
+++ zichkoding-portfolio/src/pages/index.tsx
@@ -1,10 +1,115 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import Header from '../components/header';
+import { useEffect, useState } from 'react';
 
-import Header from '../components/header'
+const inter = Inter({ subsets: ['latin'] });
 
-const inter = Inter({ subsets: ['latin'] })
+type ScrollY = {
+  scrollY: number;
+  newScrollNum: number;
+};
 
 export default function Home() {
+  const [prevScrollY, setPrevScrollY] = useState<ScrollY>({
+    scrollY: 0,
+    newScrollNum: 0,
+  });
+  const [isNum, setIsNum] = useState<ScrollY>({ newScrollNum: 0, scrollY: 0 });
+  const [stars, setStars] = useState<HTMLDivElement[]>([]);
+
+  const generateStars = () => {
+    const container = document.getElementById('space-background');
+    if (container) {
+      const starCount = 25; // Adjust the number of stars as desired (reduced to a quarter)
+      const newStars: HTMLDivElement[] = [];
+      for (let i = 0; i < starCount; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.top = `${Math.random() * 100}%`;
+        container.appendChild(star);
+        newStars.push(star);
+      }
+      setStars(newStars);
+    }
+  };
+
+  useEffect(() => {
+    generateStars();
+
+    const moveStars = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > prevScrollY.scrollY) {
+        setStars((prevStars) => {
+          const updatedStars = prevStars.map((star) => {
+            const top = parseInt(star.style.top || '0');
+            star.style.top = `${top + 5}px`; // Increased the speed of downward movement
+            if (top > window.innerHeight) {
+              star.remove();
+              return null;
+            }
+            return star;
+          });
+          return updatedStars.filter((star) => star !== null) as HTMLDivElement[];
+        });
+
+        const container = document.getElementById('space-background');
+        if (container) {
+          const newStars: HTMLDivElement[] = [];
+          let starCount = Math.floor(Math.random() * 3) + 1; // Reduced the number of stars generated
+          for (let i = 0; i < starCount; i++) {
+            const star = document.createElement('div');
+            star.className = 'star';
+            star.style.left = `${Math.random() * 100}%`;
+            star.style.top = `${-star.offsetHeight}px`;
+            container.appendChild(star);
+            newStars.push(star);
+          }
+          setStars((prevStars) => [...prevStars, ...newStars]);
+        }
+      } else if (currentScrollY < prevScrollY.scrollY) {
+        setStars((prevStars) => {
+          const updatedStars = prevStars.map((star) => {
+            const top = parseInt(star.style.top || '0');
+            star.style.top = `${top - 5}px`; // Increased the speed of upward movement
+            if (top < -star.offsetHeight) {
+              star.remove();
+              return null;
+            }
+            return star;
+          });
+          return updatedStars.filter((star) => star !== null) as HTMLDivElement[];
+        });
+
+        const container = document.getElementById('space-background');
+        if (container) {
+          const newStars: HTMLDivElement[] = [];
+          let starCount = Math.floor(Math.random() * 3) + 1; // Reduced the number of stars generated
+          for (let i = 0; i < starCount; i++) {
+            const star = document.createElement('div');
+            star.className = 'star';
+            star.style.left = `${Math.random() * 100}%`;
+            star.style.top = `${window.innerHeight + star.offsetHeight}px`;
+            container.appendChild(star);
+            newStars.push(star);
+          }
+          setStars((prevStars) => [...prevStars, ...newStars]);
+        }
+      }
+
+      setPrevScrollY((prevScrollY) => ({
+        ...prevScrollY,
+        scrollY: currentScrollY,
+      }));
+    };
+
+    window.addEventListener('scroll', moveStars);
+
+    return () => {
+      window.removeEventListener('scroll', moveStars);
+    };
+  }, []);
 
   return (
     <>
@@ -16,7 +121,7 @@ export default function Home() {
         <p className="text-xl text-center">
           Feel free to look at my bio, projects, and resume! If you want to contact me, you can do so through the contact page. If you want a quick overview of my skills, just scroll down!
         </p>
-        <div className="flex flex-col">
+        <div id="space-background" className="flex flex-col">
           <div className="flex flex-row text-xl text-center m-9">
             Item 1
           </div>
@@ -27,11 +132,8 @@ export default function Home() {
           <div className="flex flex-row text-xl text-center m-9">
             Item 3
           </div>
-          
         </div>
-
-
       </main>
     </>
-  )
+  );
 }
